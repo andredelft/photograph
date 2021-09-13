115232cdc28c147a94b331e9db2ea6ebb26956ce
André van Delft <andrevandelft@outlook.com>
2021-09-13 22:39:27 +0200

    Link carousel page and more styling

--- photograph/views.py
+++ photograph/views.py
@@ -5,19 +5,32 @@ from .models import Collection
 
 def home(request):
     return render(request, 'home.html', context={
-        'collections': Collection.objects.all()
+        'collections': Collection.objects.order_by('date_from')
     })
 
 
 def collection(request, slug):
-    collection_obj = get_object_or_404(Collection, slug=slug)
     return render(request, 'collection.html', context={
-        'collection': collection_obj
+        'collection': get_object_or_404(Collection, slug=slug)
     })
 
 
 def carousel(request, slug):
     collection_obj = get_object_or_404(Collection, slug=slug)
+
+    # We query this now to compare its count to start_carousel value
+    collection_photos = collection_obj.photo_set.all()
+
+    try:
+        start_carousel = int(request.GET.get('start', '1'))
+    except ValueError:
+        start_carousel = 1
+    else:
+        if start_carousel > collection_photos.count():
+            start_carousel = 1
+
     return render(request, 'carousel.html', context={
-        'collection': collection_obj
+        'collection': collection_obj,
+        'start_carousel': start_carousel,
+        'collection_photos': collection_photos
     })
